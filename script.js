996d80694710a33cd1e3f360d946cdf193bb5d3e
Ishita Sharma <66726780+Ishita2711@users.noreply.github.com>
2022-03-12 18:31:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 const date = new Date();
+
 const renderCalender = () => {
-
-    // date.setDate(1);
-
-    const monthDays = document.querySelector(".days");
-    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-    const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
-    const firstDayIndex = date.getDay();
-    const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
-    const nextDays = 7 - lastDayIndex - 1;
 
     const months = [
         "January",
@@ -24,8 +16,17 @@ const renderCalender = () => {
         "November",
         "December"
     ];
+
+
+    const monthDays = document.querySelector(".days");
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+    const firstDayIndex = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+    const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+    const nextDays = 7 - lastDayIndex - 1;
+
     document.querySelector(".date h1").innerHTML = months[date.getMonth()];
-    document.querySelector('.date p').innerHTML = "Today is : "+new Date().toDateString();
+    document.querySelector('.date p').innerHTML = "Today is : " + new Date().toDateString();
     let days = "";
 
     for (let x = firstDayIndex; x > 0; x--) {
